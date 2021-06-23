cc030ebf1590b88fcd785c7b9393e91ddba24be1
karl <karl.bittner@hexaquo.at>
2021-06-23 13:31:31 +0200

    Some cleanup and bugfixing

--- Water.gd
+++ Water.gd
@@ -1,16 +1,20 @@
 extends Spatial
 
+var first_frame := true
 
-var using_second_viewport := false
+var frame_number := 0
 
 
 func _ready():
 	# Set an initial "previous frame"
 	var first_image = Image.new()
 	
-	first_image.create(1, 1, false, Image.FORMAT_R8)
+	first_image.create(2, 2, false, Image.FORMAT_RGBA8)
 	first_image.lock()
-	first_image.set_pixel(0, 0, Color(1.0, 1.0, 1.0))
+	first_image.set_pixel(0, 0, Color(0.0, 0.0, 0.0))
+	first_image.set_pixel(0, 1, Color(0.0, 0.0, 0.0))
+	first_image.set_pixel(1, 0, Color(0.0, 0.0, 0.0))
+	first_image.set_pixel(1, 1, Color(1.0, 0.0, 0.0))
 	first_image.unlock()
 	
 	var first_texture = ImageTexture.new()
@@ -20,8 +24,13 @@ func _ready():
 
 
 func _process(delta):
+	# Skip the first frame because that would swap around buffers that aren't properly rendered yet
+	if first_frame:
+		first_frame = false
+		return
+	
 	# Get result of previous frame
-	var result = $WaterHeights.get_texture(using_second_viewport)
+	var result = $WaterHeights.get_texture()
 	
 	# Set it as the data of the water mesh
 	$WaterMesh.material_override.set_shader_param("water_heights", result)
@@ -31,3 +40,7 @@ func _process(delta):
 	previous_frame.create_from_image(result.get_data())
 	
 	$WaterHeights.set_previous_texture(previous_frame)
+	
+	if false:
+		previous_frame.get_data().save_png("res://frame%s.png" % [frame_number])
+		frame_number += 1
--- WaterHeights.gd
+++ WaterHeights.gd
@@ -1,10 +1,8 @@
 extends Node
 
 
-func get_texture(second_viewport: bool):
-	var height_viewport = $WaterHeightViewport2 if second_viewport else $WaterHeightViewport
-	
-	return height_viewport.get_texture()
+func get_texture():
+	return $WaterHeightViewport.get_texture()
 
 
 func set_previous_texture(texture):
